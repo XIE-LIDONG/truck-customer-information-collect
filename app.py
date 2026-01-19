602b67e722d06f058e35c47a9c61a9523e84100e
XIE LIDONG <161944504+XIE-LIDONG@users.noreply.github.com>
2026-01-19 11:43:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,16 +7,16 @@ import os
 CAR_PDF_MASTER = {
     # 1. 4x2 Tractor - 4x2 رأس جرار
     "4x2 Tractor Head | رأس جرار 4x2": {
-        "4x2 Tractor AMT High Roof Air Suspension 420HP | رأس جرار 4x2 AMT سقف عالي مع تعليق هوائي 420 حصان": 
+        "4x2 Tractor AMT Mega Air Suspension 420HP | رأس جرار 4x2 AMT سقف عالي مع تعليق هوائي 420 حصان": 
             "JH6_4x2_AMT_High_Roof_Standard_Tractor_Air_Suspension_420HP.pdf",
 
-        "4x2 Tractor AMT High Roof Leaf Spring 420HP | رأس جرار 4x2 AMT سقف عالي مع نوابض ورقية 420 حصان": 
+        "4x2 Tractor AMT Mega Leaf Spring 420HP | رأس جرار 4x2 AMT سقف عالي مع نوابض ورقية 420 حصان": 
             "JH6_4x2_AMT_High_Roof_Multifunction_Tractor_Leaf_Spring_420HP.pdf",
 
-        "4x2 Tractor AMT Flat Roof Leaf Spring 420HP | رأس جرار 4x2 AMT سقف مسطح مع نوابض ورقية 420 حصان": 
+        "4x2 Tractor AMT Half Mega Leaf Spring 420HP | رأس جرار 4x2 AMT سقف مسطح مع نوابض ورقية 420 حصان": 
             "JH6_4x2_AMT_Flat_Roof_Multifunction_Tractor_Leaf_Spring_420HP.pdf",
 
-        "4x2 Tractor MT Flat Roof Leaf Spring 420HP | رأس جرار 4x2 MT سقف مسطح مع نوابض ورقية 420 حصان": 
+        "4x2 Tractor MT Half Mega Leaf Spring 420HP | رأس جرار 4x2 MT سقف مسطح مع نوابض ورقية 420 حصان": 
             "JH6_4x2_MT_Flat_Roof_Multifunction_Tractor_Leaf_Spring_420HP.pdf"
     },
 
